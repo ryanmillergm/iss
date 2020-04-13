e57cb30a46b8b388c3b5024f44f555c406cb8522
Ryan Miller <ryanmillergm@gmail.com>
2020-04-13 16:58:57 -0600

    Refactors iss home action to implement iss facade and service

--- app/controllers/iss_controller.rb
+++ app/controllers/iss_controller.rb
@@ -1,17 +1,22 @@
 class IssController < ApplicationController
   def home
 
-    @conn = Faraday.new(url: "http://api.open-notify.org") do |faraday|
-      faraday.adapter Faraday.default_adapter
-    end
+    # @conn = Faraday.new(url: "http://api.open-notify.org") do |faraday|
+    #   faraday.adapter Faraday.default_adapter
+    # end
+    #
+    # current_location = @conn.get("/iss-now.json")
+    # current_location_results = JSON.parse(current_location.body, symbolize_names: true)
+    #
+    # passover_time = @conn.get("/iss-pass.json?lat=45.0&lon=-122.3")
+    # passover_time_results = JSON.parse(passover_time.body, symbolize_names: true)
+    #
+    # astronauts = @conn.get("/astros.json")
+    # astronauts_results = JSON.parse(astronauts.body, symbolize_names: true)
 
-    current_location = @conn.get("/iss-now.json")
-    current_location_results = JSON.parse(current_location.body, symbolize_names: true)
-
-    passover_time = @conn.get("/iss-pass.json?lat=45.0&lon=-122.3")
-    passover_time_results = JSON.parse(passover_time.body, symbolize_names: true)
-
-    astronauts = @conn.get("/astros.json")
-    astronauts_results = JSON.parse(astronauts.body, symbolize_names: true)
+    iss = IssFacade.new
+    @current_location = iss.current_location
+    @passover_time = iss.passover_time(lat = 45.0, lon = 122.3)
+    @astronauts = iss.astronauts[:people]
   end
 end
