fae7bb5d77e67f10e4190d49720d6168162b66fb
Ryan Miller <ryanmillergm@gmail.com>
2020-04-13 16:07:04 -0600

    Implements working ISS API

--- app/controllers/iss_controller.rb
+++ app/controllers/iss_controller.rb
@@ -1,4 +1,17 @@
 class IssController < ApplicationController
   def home
+
+    @conn = Faraday.new(url: "http://api.open-notify.org") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+
+    current_location = @conn.get("/iss-now.json")
+    current_location_results = JSON.parse(current_location.body, symbolize_names: true)
+
+    passover_time = @conn.get("/iss-pass.json?lat=45.0&lon=-122.3")
+    passover_time_results = JSON.parse(passover_time.body, symbolize_names: true)
+
+    astronauts = @conn.get("/astros.json")
+    astronauts_results = JSON.parse(astronauts.body, symbolize_names: true)
   end
 end
