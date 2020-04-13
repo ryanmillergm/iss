e0496a04935119aa53f9e8c4a42db3f87f99e86c
Ryan Miller <ryanmillergm@gmail.com>
2020-04-13 15:49:57 -0600

    Adds basic ISS home template and updated spec

--- spec/features/user_can_get_iss_information_spec.rb
+++ spec/features/user_can_get_iss_information_spec.rb
@@ -1,41 +1,39 @@
 require 'rails_helper'
 
-feature "User can visit root page" do
-  scenario "and get ISS's current location" do
-    visit "/"
-    within(".current-location") do
-      expect(page).to have_content("Current Location:")
-    end
-  end
 
-  feature "User can get time when it will pass over a location" do
-    scenario "fill in latitude and longitue and submit" do
-      visit '/'
+RSpec.describe "As a visitor" do
+  describe "and I visit the home page" do
+    it  "I can get ISS's current location" do
 
-      fill_in "latitude", with: "45"
-      fill_in "latitude", with: "122.3"
-
-      click_button "Get Pass Over Time"
-
-      expect(current_path).to eq(root_path)
-
-      within('.passover-time')
-      expect(page).to have_content("Duration: ")
-      expect(page).to have_content("Rise Time:")
-    end
-  end
-
-  feature "User get details on individuals in space" do
-    scenario "click on see astronauts" do
       visit "/"
 
-      click_on "See Astronauts"
-
-      expect(current_path).to eq(root_path)
-
-      within('.astronaut-1')
-      expect(page).to have_content("Name: ")
-      expect(page).to have_content("Craft:")
+      within(".current-location") do
+        expect(page).to have_content("Current Location:")
       end
     end
+
+    it  "I can ISS estimated passover time and duration" do
+
+      visit "/"
+
+      within(".passover-time") do
+        expect(page).to have_content("Duration:")
+        expect(page).to have_content("Rise Time:")
+      end
+    end
+
+    it "I can get details on individuals in space" do
+
+      visit "/"
+    #
+    #     # click_on "See Astronauts"
+    #     #
+    #     # expect(current_path).to eq(root_path)
+    #
+      within(first(".astronaut")) do
+        expect(page).to have_content("Name:")
+        expect(page).to have_content("Craft:")
+      end
+    end
+  end
 end
