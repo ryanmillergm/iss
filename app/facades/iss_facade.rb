861f4fa3ca0daa6fc162b7f4393fcb1644a51942
Ryan Miller <ryanmillergm@gmail.com>
2020-04-15 00:21:07 -0600

    Refactors iss facade to create instances

--- app/facades/iss_facade.rb
+++ app/facades/iss_facade.rb
@@ -1,15 +1,22 @@
 class IssFacade
   def current_location
-    iss_service.iss_current_location
+    iss_current_location_data = iss_service.iss_current_location
+    CurrentLocation.new(iss_current_location_data[:iss_position], iss_current_location_data[:timestamp])
   end
 
   def passover_time(lat, lon)
-    iss_service.expected_iss_passover_time(lat, lon)
+    passover_data = iss_service.expected_iss_passover_time(lat, lon)[:response].first
+    PassoverTime.new(passover_data)
   end
 
   def astronauts
-    iss_service.astronaut_list
+    astronaut_data = iss_service.astronaut_list
+    astronaut_data[:people].map do |astronaut|
+      Astronaut.new(astronaut)
+    end
   end
+
+  private
 
   def iss_service
     @iss ||= IssService.new
